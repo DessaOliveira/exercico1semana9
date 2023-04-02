57f8ac363a8e4000bc89db190d83361e3333907c
Andressa Oliveira <117666961+DessaOliveira@users.noreply.github.com>
2023-04-02 18:36:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ app.post("/places", async (request, response) => {
     const place = await Place.create(data);
     response.status(201).json(place);
   } catch (error) { console.log(error)
+       response
+      .status(500)
+      .json({ message: "Não foi possivel concluir a operação" });        
   
   }
 });
