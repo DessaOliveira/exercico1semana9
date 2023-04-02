c037ff5049fd87caf6927c3cf63508c2e04aacf3
Andressa <dessa_trindade12@hotmail.com>
2023-04-02 17:55:28 -0300

    adicionar

--- index.js
+++ index.js
@@ -3,26 +3,30 @@ const connection = require("./src/database");
 const Place = require("./src/models/place");
 
 const app = express();
+app.use(express.json());
 
-connection.authenticate()
-connection.sync({alter:true})
+connection.authenticate();
+connection.sync({ alter: true });
 
+app.post("/places", async (request, response) => {
+  try {
+    const data = {
+      name: request.body.name,
+      contact: request.body.contact,
+      opening_hours: request.body.opening_hours,
+      description: request.body.description,
+      latitude: request.body.latitude,
+      longitude: request.body.longitude,
+    };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    const place = await Place.create(data);
+    response.status(201).json(place);
+  } catch (error) { console.log(error)
+    response
+      .status(500)
+      .json({ message: "Não foi possivel concluir a operação" });
+  }
+});
 
 app.listen(9999, () => {
   console.log("Servidor online");
