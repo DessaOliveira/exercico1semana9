70a4239c1e19de74a73972bc46a678258778ef23
Andressa <dessa_trindade12@hotmail.com>
2023-04-08 10:05:33 -0300

    adicionar

--- index.js
+++ index.js
@@ -54,6 +54,28 @@ app.delete("/places/:id", async (request, response) => {
     }
   });
 
+  app.put("/places/:id", async (request, response) => {
+    try {
+      const placeInDatabase = await Place.findByPk(request.params.id); 
+  
+      if (!placeInDatabase) {
+        return response.status(404).json({ message: "Solicitação não encontrada" });
+      }
+  
+      placeInDatabase.contact = request.body.contact;
+      placeInDatabase.description = request.body.description;
+  
+      await placeInDatabase.save(); 
+  
+      response.json(placeInDatabase);
+    } catch (error) {
+      console.log(error);
+      response
+        .status(500)
+        .json({ message: "Não conseguimos processar sua solicitação." });
+    }
+  });
+
 
 
 app.listen(9999, () => {
