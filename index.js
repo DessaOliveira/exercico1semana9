04a5824675fd993ef574bd4403f6c59e9fdc24cc
Andressa <dessa_trindade12@hotmail.com>
2023-04-12 16:24:02 -0300

    adicionar token

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+
 const connection = require("./src/database");
+const validateToken = require('./src/middlewares/validate-token')
+
 const Place = require("./src/models/place");
 const User = require('./src/models/users')
 
@@ -80,23 +86,79 @@ app.delete("/places/:id", async (request, response) => {
   app.post('/users', async (request, response) => {
 
     try {
-         const user = {
-
+      const userInDatabase = await User.findOne({
+        where: { username: request.body.username },
+      });
+  
+      if (userInDatabase) {
+        return response
+          .status(409)
+          .json({ message: "Já existe um usuário com essa conta." });
+      }
+  
+      const hash = await bcrypt.hash(request.body.password, 10); 
+      const newUser = {
         name: request.body.name,
         email: request.body.email,
         username: request.body.username,
-        password: request.body.password,
- 
-      }
-
-         const newUser = await User.create(user)
-
-             response.status(201).json(newUser)
-
+        password: hash,
+      };
+  
+  
+      const user = await User.create(newUser);
+      const {password, ...rest} = user.toJSON()
+  
+      response.status(201).json(rest);
+  
     } catch (error) {
-        response.status(500)
+      response
+        .status(500)
         .json({ message: "Não conseguimos processar sua solicitação." });
     }
+  }); 
+  
+  app.post('/users/login', async (request, response) => {
+  
+    try {
+      const userInDatabase = await User.findOne({
+        where: {
+          username: request.body.username,
+        },
+      });
+      if (!userInDatabase) {
+        return response.status(404).json({ message: "Credenciais  invalidas!" });
+      }
+      const passwordIsValid = await bcrypt.compare(
+        request.body.password,
+        userInDatabase.password
+      );
+      if (!passwordIsValid) {
+        console.log(error)
+        return response
+          .status(404)
+          .json({ message: "Credenciais incorretas" });
+      }
+  
+      const token = jwt.sign(
+  
+        {
+          id: userInDatabase.id,
+        },
+        "GUNS_N_ROSES",
+        {
+          expiresIn: '1h'
+        }
+      )
+      response.json({ username: userInDatabase.username,  token });
+
+    } catch (error) { 
+
+      response
+        .status(500)
+        .json({ message: "Não conseguimos processar sua solicitação." });
+    }
+  
+  
   });
 
 app.listen(9999, () => {
